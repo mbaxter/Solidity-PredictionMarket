4fda515c87522eda47af0a155ee0650b16bd2188
mbaxter <meredith.a.baxter@gmail.com>
2017-09-10 23:43:45 -0400

    Mess with loader.

--- src/components/pure/loader.js
+++ src/components/pure/loader.js
@@ -1,16 +1,18 @@
 "use strict";
 require('./loader.css');
 require('../../styles/fx/shadow.css');
+require('../../styles/fx/glow.css');
+require('../../styles/fx/fade.css');
 const React = require('react');
 
 const Loader = ({ isEnabled }) => {
     return (
-        <div className={isEnabled ? "" : "d-none"}>
+        <div className={`fade ${isEnabled ? "show" : "hide"}`}>
             <div className="spinner">
                 <div className="spinner-bars">
-                    <div className="spinner-bar spinner-bar1 shadow"></div>
-                    <div className="spinner-bar spinner-bar2 shadow"></div>
-                    <div className="spinner-bar spinner-bar3 shadow"></div>
+                    <div className="spinner-bar spinner-bar1 glow"></div>
+                    <div className="spinner-bar spinner-bar2 glow"></div>
+                    <div className="spinner-bar spinner-bar3 glow"></div>
                 </div>
             </div>
             <div className="spinner">
